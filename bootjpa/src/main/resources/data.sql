e4cc936a7476a18c8eb9a8eca8bdef66bc233ecd
56233 <56233@LTCH-5CD105HTBJ.corp.hexaware.com>
2021-06-26 19:00:44 +0530

    108 inserted

--- bootjpa/src/main/resources/data.sql
+++ bootjpa/src/main/resources/data.sql
@@ -3,3 +3,6 @@ insert into alien values (102,'Kumar');
 insert into alien values (103,'Nandagopal');
 insert into alien values (104,'HexEmp');
 insert into alien values (105,'Chennai');
+insert into alien values (106,'CICD');
+insert into alien values (107,'Example');
+insert into alien values (108,'Bamboo');
