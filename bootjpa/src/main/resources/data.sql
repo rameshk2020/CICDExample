aec0c9147598f79e0a136d064bd585318c30fd82
56233 <56233@LTCH-5CD105HTBJ.corp.hexaware.com>
2021-06-26 20:54:52 +0530

    105 record added

--- bootjpa/src/main/resources/data.sql
+++ bootjpa/src/main/resources/data.sql
@@ -2,3 +2,4 @@ insert into alien values (101,'Ramesh');
 insert into alien values (102,'Kumar');
 insert into alien values (103,'Nandagopal');
 insert into alien values (104,'GPS');
+insert into alien values (105,'OIT');
