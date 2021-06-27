7d0de93a31223397f587f868e71305f89d44033e
56233 <56233@LTCH-5CD105HTBJ.corp.hexaware.com>
2021-06-27 20:05:39 +0530

    data outside bin

--- bootjpa/src/main/resources/data.sql
+++ bootjpa/src/main/resources/data.sql
@@ -1,5 +1,4 @@
 insert into alien values (101,'Ramesh');
 insert into alien values (102,'Kumar');
-insert into alien values (103,'Nandagopal');
-insert into alien values (104,'GPS');
+insert into alien values (201,'GPS');
 
