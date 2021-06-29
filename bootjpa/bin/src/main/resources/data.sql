ff62d93de6d0fe88b4f7e2161ea926a8c7b92b07
Ramesh <rams.nandagopal@gmail.com>
2021-06-29 10:53:20 +0530

    202 inserted

--- bootjpa/bin/src/main/resources/data.sql
+++ bootjpa/bin/src/main/resources/data.sql
@@ -1,1 +1,6 @@
 insert into alien values (101,'Ramesh');
+insert into alien values (102,'Kumar');
+insert into alien values (103,'Nandagopal');
+insert into alien values (104,'Chennai');
+insert into alien values (201,'GPS');
+
